a53d88792a5407d78633b35a46e36cffdfda12dd
atayiilmaz <yiilmazataberk@gmail.com>
2024-10-17 17:11:09 +0300

    feat: add some optional parameters for command-line

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from video_processing.file_loader import load_images_from_folder, load_titles_from_excel
 from video_processing.overlay import create_localized_videos
 
@@ -7,16 +8,20 @@ IMAGES_FOLDER = "assets/images/"
 EXCEL_FILE_PATH = "assets/titles.xlsx"
 OUTPUT_DIR = "output/"
 
-def main():
-    # Ensure output directory exists
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
-    
+def main(args):
     # Load images and titles
     images = load_images_from_folder(IMAGES_FOLDER)
     titles = load_titles_from_excel(EXCEL_FILE_PATH)
     
+    os.makedirs(args.output_dir, exist_ok=True)
+
     # Create localized videos
-    create_localized_videos(BASE_VIDEO_PATH, images, titles, OUTPUT_DIR)
+    create_localized_videos(BASE_VIDEO_PATH, images, titles, args.font_size, args.output_dir)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Create localized videos by overlaying images and titles on a base video.")
+    parser.add_argument("--output_dir", type=str, default=OUTPUT_DIR, help="Output directory for localized videos.")
+    parser.add_argument("--font_size", type=int, default=45, help="Font size for title text.")
+    
+    args = parser.parse_args()
+    main(args)
